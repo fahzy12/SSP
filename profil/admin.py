b4f9af9e343ce716930da53ae4955204ab58ca88
anoercholis <anoercholis@asia.ac.id>
2025-06-09 17:23:59 +0700

    belajar model

--- profil/admin.py
+++ profil/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Barang, Jenis
+
+class BarangAdmin(admin.ModelAdmin):
+    list_display = ['kdbrg', 'nama','stok','harga','jenis_id']
+    search_fields = ('kdbrg','nama','jenis_id__nama')
+    list_filter = ['jenis_id',]
+    list_per_page = 3
+
+
+admin.site.register(Barang, BarangAdmin)
+admin.site.register(Jenis)
